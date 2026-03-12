2cfc4cf0b5bfc445d1aa02666c4aa4a3889f5e1e
Guilherme Eduardo <168122808+GuiDev005@users.noreply.github.com>
2026-03-12 21:45:06 +0000

    .

--- atividades/vetormedia.c
+++ atividades/vetormedia.c
@@ -2,14 +2,14 @@
 
 int main(){
 
-    float nota, media = 0, nota1[30], nota2[30], MediaAluno[30];
+    double nota, media = 0, nota1[30], nota2[30], MediaAluno[30];
 
     int ContVetor = 0, i = 1;
     
     while(i < 31){
 
         printf("Digite a nota 1 do %d° aluno :", i);
-        if (scanf("%f", &nota) == 0){
+        if (scanf("%lf", &nota) == 0 || nota < 0){
             printf("Nota inválida!");
             while(getchar() != '\n');
             continue;
@@ -18,12 +18,12 @@ int main(){
             nota1[ContVetor] = nota;
         }
         printf("Digite a nota 2 do %d° aluno :", i);
-        if (scanf("%f", &nota) == 0){
+        if (scanf("%lf", &nota) == 0 || nota < 0){
             printf("Nota inválida, digite novamente!");
             while(getchar() != '\n');
             while (1){
                 printf("Digite a nota 2 do %d° aluno :", i);
-                if (scanf("%f", &nota) == 0){
+                if (scanf("%lf", &nota) == 0 || nota < 0){
                     printf("Nota inválida, digite novamente!");
                     while(getchar() != '\n');
                     continue;
@@ -46,10 +46,10 @@ int main(){
     ContVetor = 0;
     i = 1;
     media =  media / 30;
-    printf("A média geral da turma foi de %.2f\n", media);
+    printf("A média geral da turma foi de %.2lf\n", media);
     while(i < 31){
         if (MediaAluno[ContVetor] > media){
-            printf("O %d° aluno teve nota 1: %.2f e nota 2: %.2f e média: %.2f e ficou acima da média da turma!\n", i, nota1[ContVetor], nota2[ContVetor], MediaAluno[ContVetor]);
+            printf("O %d° aluno teve nota 1: %.2lf e nota 2: %.2lf e média: %.2lf e ficou acima da média da turma!\n", i, nota1[ContVetor], nota2[ContVetor], MediaAluno[ContVetor]);
         }
       
         i += 1;
